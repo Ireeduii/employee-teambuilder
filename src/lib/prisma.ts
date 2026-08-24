892d0bffadf50971d9583398ad6c67c37d75b965
Ireeduii <25LP4866@nest.edu.mn>
2026-08-25 00:34:46 +0800

    fix: lib/prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,33 +1,53 @@
+// // import { PrismaClient } from "@prisma/client";
+// // import { PrismaPg } from "@prisma/adapter-pg";
+// // import { Pool } from "pg";
+
+// // const connectionString = process.env.DATABASE_URL;
+
+// // const pool = new Pool({ connectionString });
+// // const adapter = new PrismaPg(pool);
+
+// // const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// // export const prisma =
+// //   globalForPrisma.prisma ||
+// //   new PrismaClient({
+// //     adapter,
+// //     log: ["query", "error"],
+// //   });
+
+// // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 // import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { Pool } from "pg";
 
-// const connectionString = process.env.DATABASE_URL;
-
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaPg(pool);
-
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
 // export const prisma =
-//   globalForPrisma.prisma ||
+//   globalForPrisma.prisma ??
 //   new PrismaClient({
-//     adapter,
-//     log: ["query", "error"],
+//     log: ["query"],
 //   });
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    adapter,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
